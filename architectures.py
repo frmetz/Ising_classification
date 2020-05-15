c273dfb3c8ca57f5c9f59bdbb3865c8ffc63012c
frmetz <friederike.metz@oist.jp>
2020-05-15 20:40:14 +0900

    Refactor architectures

--- architectures.py
+++ architectures.py
@@ -1,44 +1,30 @@
-########################################
-# copied and modified from: https://github.com/google/jax/issues/1393
-########################################
+################################################################################
+# based on: https://github.com/google/jax/issues/1393
+################################################################################
 
 
-from jax.config import config
+#from jax.config import config
 #config.update("jax_enable_x64", True)
-from jax import jit, device_put
-#from jax.experimental.stax import relu, BatchNorm
 
-import functools
 import itertools
 from jax import lax
 from jax import random
 import jax.numpy as jnp
 import numpy as np
 
-from jax.nn import (relu, log_softmax, softmax, softplus, sigmoid, elu,
-                    leaky_relu, selu, gelu, normalize)
 from jax.nn.initializers import glorot_normal, normal, ones, zeros
 
-# aliases for backwards compatibility
-# glorot = glorot_normal
-# randn = normal
-# logsoftmax = log_softmax
 
-
-
-
-def periodic_padding(inputs,filter_shape,strides):
-    n_x=filter_shape[0]-strides[0]
-    n_y=filter_shape[1]-strides[1]
-    #return jnp.pad(inputs, ((0,0),(0,0),(0,n_x),(0,n_y)), mode='wrap')
-    return jnp.pad(inputs, ((0,0),(0,n_x),(0,n_y),(0,0)), mode='wrap')
-
-
+def periodic_padding(inputs, filter_shape, strides):
+    n_x = filter_shape[0]-strides[0]
+    n_y = filter_shape[1]-strides[1]
+    # return jnp.pad(inputs, ((0,0),(0,0),(0,n_x),(0,n_y)), mode='wrap')
+    return jnp.pad(inputs, ((0, 0), (0, n_x), (0, n_y), (0, 0)), mode='wrap')
 
 
 def PeriodicConv(out_chan, filter_shape,
-                strides=None, padding='VALID', dimension_numbers = ('NHWC', 'HWIO', 'NHWC'), W_init=None,
-                b_init=normal(1e-6), ignore_b=False, dtype=jnp.float64):
+                 strides=None, padding='VALID', dimension_numbers=('NHWC', 'HWIO', 'NHWC'), W_init=None,
+                 b_init=normal(1e-6), ignore_b=False, dtype=jnp.float64):
     """Layer construction function for a general convolution layer."""
     lhs_spec, rhs_spec, out_spec = dimension_numbers
     one = (1,) * len(filter_shape)
@@ -49,8 +35,8 @@ def PeriodicConv(out_chan, filter_shape,
 
         # add padding dimensions for periodic BC; move this line into conv_general_shape_tuple after defining padding='PERIODIC'
 
-        #input_shape+=np.array((0,0)+strides) # new
-        input_shape+=np.array([0]+list(strides)+[0]) # new
+        # input_shape+=np.array((0,0)+strides) # new
+        input_shape += np.array([0]+list(strides)+[0])  # new
 
         filter_shape_iter = iter(filter_shape)
         kernel_shape = [out_chan if c == 'O' else
@@ -74,8 +60,8 @@ def PeriodicConv(out_chan, filter_shape,
     def apply_fun(params, inputs, **kwargs):
 
         # move this line into lax.conv_general_dilated after defining padding='PERIODIC'
-        inputs=periodic_padding(inputs.astype(dtype),filter_shape,strides)
-        #print(inputs.shape)
+        inputs = periodic_padding(inputs.astype(dtype), filter_shape, strides)
+        # print(inputs.shape)
         if not ignore_b:
             W, b = params
             return lax.conv_general_dilated(inputs, W, strides, padding, one, one,
@@ -84,6 +70,5 @@ def PeriodicConv(out_chan, filter_shape,
             W = params
             return lax.conv_general_dilated(inputs, W, strides, padding, one, one,
                                             dimension_numbers)
-
 
     return init_fun, apply_fun
